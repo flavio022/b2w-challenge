0153d9ce0fa1c64649580395ee3767ecabb742cd
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2021-05-06 14:48:25 -0300

    Adding Entities for response of api and POJO for datas of planets

--- src/main/java/com/b2w/planetapi/entities/ResultApi.java
+++ src/main/java/com/b2w/planetapi/entities/ResultApi.java
@@ -1,2 +1,22 @@
-package com.b2w.planetapi.entities;public class ResultApi {
+package com.b2w.planetapi.entities;
+
+import java.util.List;
+
+public class ResultApi {
+    private List<StarWarsPlanet> results;
+
+    public ResultApi() {
+    }
+
+    public ResultApi(List<StarWarsPlanet> results, String name) {
+        this.results = results;
+    }
+
+    public List<StarWarsPlanet> getResults() {
+        return results;
+    }
+
+    public void setResults(List<StarWarsPlanet> results) {
+        this.results = results;
+    }
 }
--- src/main/java/com/b2w/planetapi/entities/StarWarsPlanet.java
+++ src/main/java/com/b2w/planetapi/entities/StarWarsPlanet.java
@@ -1,2 +1,45 @@
-package com.b2w.planetapi.entities;public class StarWarsPlanet {
+package com.b2w.planetapi.entities;
+import java.util.List;
+import java.util.Objects;
+
+public class StarWarsPlanet {
+    private String name;
+    private List <String> films;
+
+    public StarWarsPlanet() {
+    }
+
+    public StarWarsPlanet(String name, List<String> films) {
+        this.name = name;
+        this.films = films;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<String> films) {
+        this.films = films;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StarWarsPlanet that = (StarWarsPlanet) o;
+        return Objects.equals(name, that.name) && Objects.equals(films, that.films);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, films);
+    }
 }
